d63a93408e9248451bab8187308018dceaa3d685
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2024-10-10 20:30:32 +0300

    math

---
+++ integer.js
@@ -0,0 +1,20 @@
+// Math 
+
+
+let chislo1,chislo2;
+
+chislo1 = 45,chislo2 = 67;
+
+
+
+console.log(chislo1+chislo2);
+console.log(chislo1*chislo2);
+console.log((chislo1/chislo2).toFixed(2));
+console.log(chislo1-chislo2);
+console.log(10%4);
+console.log(2**8);
+
+console.log(Math.pow(2,16));
+console.log(Math.trunc(Math.random()*100));
+
+
